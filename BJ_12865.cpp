84458ae4b54e1599c46c047b691cc4675808ad18
은종 <eunjong@eunjong-ui-MacBookPro.local>
2021-02-22 23:46:14 +0900

    BJ_12865 solved

---
+++ BJ_12865.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+  int n, k;
+  scanf("%d %d", &n, &k);
+  int w_list[n+1];
+  int v_list[n+1];
+  int record[k+1][n+1];
+  memset(record, 0, sizeof(record));
+  int i,j;
+  for(i=1; i<=n; i++) scanf("%d %d", &w_list[i], &v_list[i]);
+
+  for(i=1; i<=k; i++)
+  {
+    for(j=1; j<=n; j++)
+    {
+      if(i<w_list[j]){
+        record[i][j] = record[i][j-1];
+        continue;
+      }
+      record[i][j] = max(record[i][j-1], record[i-w_list[j]][j-1] +v_list[j]);
+    }
+  }
+
+  printf("%d", record[k][n]);
+}
