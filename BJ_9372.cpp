39332de4577a920a8432797993fd0c1ccb120207
은종 <eunjong55@naver.com>
2021-04-06 00:36:42 +0900

    BJ9372_solved

---
+++ BJ_9372.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+  int t;
+  int n, m;
+  scanf("%d", &t);
+  for(int i=0; i<t; i++)
+  {
+    scanf("%d %d", &n, &m);
+    for(int j=0; j<m; j++)
+    {
+      int temp1, temp2;
+      scanf("%d %d", &temp1, &temp2);
+    }
+    printf("%d\n", n-1);
+  }
+}
+
